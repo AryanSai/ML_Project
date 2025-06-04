78b3e4caf7e09d4e15536ad2c81a2727828c2808
Aryan Sai Arvapelly <aryansaiarvapelly@gmail.com>
2025-06-04 18:42:15 +0530

    flask app

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,11 @@ def save_object(file_path, obj):
     except ImportError:
         print("os module is not available. Cannot create directories.")
         return
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
